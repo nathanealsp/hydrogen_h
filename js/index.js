4025ca30a6ab503de9e37f06d521a8a0fcc4d1e4
Nytheneals <nythes@gmail.com>
2018-08-20 12:47:39 -0400

    Implemented the delete functionality

--- js/index.js
+++ js/index.js
@@ -14,19 +14,34 @@ capitalize('rarrdds');
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const input = form.querySelector('input');
-const newLi = document.createElement('li');
-const
-  // ADDING ELEMENT
-  // TIMELINE
-  form.addEventListener('submit', e => {
-    e.preventDefault();
 
-    const newText = capitalize(input.value);
-    input.value = '';
-    if (newLi.innerHTML !== newText) {
-      newLi.innerHTML = newText;
-      return ul.appendChild(newLi);
+// ADDING ELEMENT
+// TIMELINE
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const newLi = document.createElement('li');
+  const newH1 = document.createElement('h1');
+  const button = document.createElement('button');
+  const span = document.createElement('span');
+  button.setAttribute('id', 'delete');
+  button.innerHTML = 'DELETE';
+  console.log(button);
+  span.appendChild(button);
+  console.log(span);
+  newLi.appendChild(newH1);
+  newLi.appendChild(span);
+  console.log(newLi);
+  const newText = capitalize(input.value);
+  input.value = '';
+  // if (newLi.innerHTML !== '') {
+  newH1.innerHTML = newText;
+  return ul.appendChild(newLi);
+  // }
+});
 
-      // return ul.appendChild(newLi);
-    }
-  });
+// DELETE METHOD
+ul.addEventListener('click', e => {
+  const toDelete = e.target.parentNode.parentNode;
+  console.log(toDelete);
+  ul.removeChild(toDelete);
+});
