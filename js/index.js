81df931ac106aab736891b584949e89dcd4fb424
Nytheneals <nythes@gmail.com>
2018-08-20 11:41:08 -0400

    Additions and Li styling:

--- js/index.js
+++ js/index.js
@@ -4,20 +4,29 @@ const log = document.querySelector('.log');
 log.innerHTML = rangeSlider.value;
 rangeSlider.addEventListener('input', () => (log.innerHTML = rangeSlider.value));
 
+function capitalize(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
+
+capitalize('rarrdds');
+
 // LIST
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const input = form.querySelector('input');
+const newLi = document.createElement('li');
+const
+  // ADDING ELEMENT
+  // TIMELINE
+  form.addEventListener('submit', e => {
+    e.preventDefault();
 
-// ADDING ELEMENT
-// TIMELINE
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const newLi = document.createElement('li');
-  const newText = input.value;
-  input.value = '';
-  // if (newLi.innerHTML !== '') {
-  newLi.innerHTML = newText;
-  ul.appendChild(newLi);
-  // }
-});
+    const newText = capitalize(input.value);
+    input.value = '';
+    if (newLi.innerHTML !== newText) {
+      newLi.innerHTML = newText;
+      return ul.appendChild(newLi);
+
+      // return ul.appendChild(newLi);
+    }
+  });
